ec526a036a8e0dac878c896365ed1d482250d044
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-02-09 17:21:10 +0530

    initial commit

---
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -0,0 +1,4 @@
+export class CreateNinjaDto {
+  name: string;
+  weapon: 'starts' | 'nunchucks';
+}
---
+++ src/ninjas/ninjas.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+
+@Injectable()
+export class NinjasService {
+  private ninjas = [
+    { id: 0, name: 'ninjaA', weapon: 'starts' },
+    { id: 0, name: 'ninjaB', weapon: 'nunchucks' },
+  ];
+
+  getNinjas(weapon?: 'starts' | 'nunchucks') {
+    if (weapon) {
+      return this.ninjas.filter((ninja) => {
+        return ninja.weapon === weapon;
+      });
+    }
+    return this.ninjas;
+  }
+
+  getNinja(id: number) {
+    const ninja = this.ninjas.find((ninja) => ninja.id === id);
+    if (!ninja) {
+      throw new Error('ninja not found');
+    }
+    return ninja;
+  }
+
+  createNinja(creaNinjaDto: CreateNinjaDto) {
+    const newNinja = {
+      ...creaNinjaDto,
+      id: Date.now(),
+    };
+    this.ninjas.push(newNinja);
+    return newNinja;
+  }
+
+  updateNinja(id: number, UpdateNinjaDto: UpdateNinjaDto) {
+    this.ninjas = this.ninjas.map((ninja) => {
+      if (ninja.id === id) {
+        return { ...ninja, ...UpdateNinjaDto };
+      }
+      return ninja;
+    });
+    return this.getNinja(id);
+  }
+
+  removeNinja(id: number) {
+    const toBeRemoved = this.getNinja(id);
+    this.ninjas = this.ninjas.filter((ninja) => ninja.id !== id);
+    return toBeRemoved;
+  }
+}
